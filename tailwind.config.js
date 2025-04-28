7e6d14f5d5454590bdda497b3fa0f8539aef4d17
amnakhalid625 <amnakhalidgsr@gmail.com>
2025-04-28 22:56:48 +0500

    elite_partnens

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,11 +21,7 @@ export default {
       secondaryText:'#000000',
       border: "#DDDDDD",
       },
-      boxShadow: {
-        "box-shadow": "0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)",
-        "box-shadow-md": "0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)",
-        "box-shadow-lg": "0 20px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04)",
-      }
+      
       
     },
   },
